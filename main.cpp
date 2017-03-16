a12049a0c0d175def9336569e8d8d7f00e0c4081
arsen <arssenk@gmail.com>
2017-03-16 09:25:40 +0200

    Boost is working properly

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <boost/signal.hpp>
 /*
   Function Declarations for builtin shell commands:
  */
@@ -140,39 +140,10 @@ int lsh_execute(char **args)
  */
 char *lsh_read_line(void)
 {
-  int bufsize = LSH_RL_BUFSIZE;
-  int position = 0;
-  char *buffer = (char *) malloc(sizeof(char) * bufsize);
-  int c;
-
-  if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    // Read a character
-    c = getchar();
-
-    // If we hit EOF, replace it with a null character and return.
-    if (c == EOF || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
-      buffer = (char *) realloc(buffer, bufsize);
-      if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-  }
+  char *line = NULL;
+  size_t bufsize = 0; // have getline allocate a buffer for us
+  getline(&line, &bufsize, stdin);
+  return line;
 }
 
 #define LSH_TOK_BUFSIZE 64
@@ -192,11 +163,11 @@ char **lsh_split_line(char *line)
     fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
-
   token = strtok(line, LSH_TOK_DELIM);
   while (token != NULL) {
     tokens[position] = token;
     position++;
+
 
     if (position >= bufsize) {
       bufsize += LSH_TOK_BUFSIZE;
