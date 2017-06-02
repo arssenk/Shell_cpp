6e9da75857192f51c06d47624fba2342d401b576
arsen <arssenk@gmail.com>
2017-06-02 13:25:02 +0300

    final version

--- main.cpp
+++ main.cpp
@@ -77,6 +77,7 @@ int lsh_pwd(char **args) {
     boost::filesystem::path full_path(boost::filesystem::current_path());
     string pwd = full_path.string();
     std::cout << pwd << std::endl;
+    return 1;
 }
 
 /**
